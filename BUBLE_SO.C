8efe0020311d48b9c4e1f40b987ddb33b30060df
Manya Desai <manyadesai30@gmail.com>
2026-02-24 17:16:03 +0530

    Add files via upload

---
+++ BUBLE_SO.C
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+#define size 10
+void main()
+{
+   int x[size],i,j,temp;
+   clrscr();
+   for(i=0;i<size;i++)
+   {
+      printf("ente4r the value %d:",i+1);
+      scanf("%d",&x[i]);
+   }
+   for(i=0;i<size;i++)
+   {
+      for(j=0;j<size-i-1;j++)
+      {
+	 if(x[j]>x[j+1])
+	 {
+	     temp=x[j];
+	     x[j]=x[j+1];
+	     x[j+1]=temp;
+	 }
+      }
+   }
+   for(i=0;i<size;i++)
+   {
+      printf("\n %d):%d",i+1,x[i]);
+   }
+   getch();
+}
